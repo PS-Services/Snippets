5b5c3446077911b42bf9ad86135d8efbd8f33276
The Sharp Ninja <ninja@thesharp.ninja>
2021-11-30 13:09:03 -0600

    Pathing

--- oh-my-posh-linux.ps1
+++ oh-my-posh-linux.ps1
@@ -2,16 +2,17 @@ param([switch]$Verbose = $false)
 if ($IsLinux) {
     try {
         $ohMyPosh = Get-Command /usr/local/bin/oh-my-posh
+        $poshThemes = "$env:USERPROFILE/.poshthemes"
 
         if (-not $ohMyPosh) {
             sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
             sudo chmod +x /usr/local/bin/oh-my-posh
-
-            mkdir ~/.poshthemes
+            
+            mkdir $poshThemes
             wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-            unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
-            chmod u+rw ~/.poshthemes/*.json
-            Remove-Item ~/.poshthemes/themes.zip
+            unzip $poshThemes/themes.zip -d ~/.poshthemes
+            chmod u+rw $poshThemes/*.json
+            Remove-Item $poshThemes/themes.zip
 
             $ohMyPosh = Get-Command /usr/local/bin/oh-my-posh
         }
@@ -22,16 +23,14 @@ if ($IsLinux) {
         else {
             Write-Verbose "`$ohMyPosh: $($ohMyPosh.Source)" -Verbose:$Verbose
 
-            $themes = '~/.poshthemes'
+            Write-Verbose "`$poshThemes: $($poshThemes)" -Verbose:$Verbose
 
-            Write-Verbose "`$themes: $($themes)" -Verbose:$Verbose
-
-            if (Test-Path $themes) {
-                oh-my-posh --init --shell pwsh --config $themes/ys.omp.json `
+            if (Test-Path $poshThemes) {
+                oh-my-posh --init --shell pwsh --config $poshThemes/ys.omp.json `
                 | Invoke-Expression
             }
             else {
-                Write-Host "Could not locate $themes"
+                Write-Host "Could not locate $poshThemes"
             }
         }
     }
