858183694dac041025306fe8d0a1cc8976869752
Payton Byrd <payton.byrd@sedgwick.com>
2021-11-30 10:48:54 -0600

    Can now run Bing from alternative location.

--- bing.ps1
+++ bing.ps1
@@ -1,17 +1,34 @@
 param([switch]$Verbose = $false)
 
 try {
-  $ApiKey = '3c7e251544ba414cbeacad9db55bdf6e'
-
-  $env:BingApiKey = $ApiKey
-
-  function Search-Bing {
-    $query = [System.String]::Join(' ', $args);
-    Write-Verbose "& dotnet-script c:\Users\kingd\bing.csx `"$query`" --max=3" -Verbose:$Verbose
-    & dotnet-script c:\Users\kingd\bing.csx "$query" --max=3
+  $dnScript = Get-Command dotnet-script
+  if (-not $dnScript) {
+    & dotnet tool install dotnet-script -g
+    $dnScript = Get-Command dotnet-script
   }
+  if (-not $dnScript) {
+    Write-Host "Cannot install dotnet-script"
+  }
+  else {
+    $env:dnscriptPath = $dnScript.Source
+    if($env:Dotnet) {
+      $env:dnscriptPath = $env:dnscriptPath.Replace("$env:USERPROFILE\.dotnet", $env:Dotnet)
+    }
+    
+    $env:BingLocation = (Get-Location).ToString()
+    if (-not $env:BingLocation.EndsWith("\Snippets")) { $env:BingLocation = Join-Path $env:BingLocation -Child "Snippets" }
+    Write-Host "`$env:BingLocation: $env:BingLocation"
+    $ApiKey = '3c7e251544ba414cbeacad9db55bdf6e'
 
-  Set-Alias -Name bing -Value Search-Bing -PassThru
+    $env:BingApiKey = $ApiKey  
+    function Search-Bing {
+      $query = [System.String]::Join(' ', $args);
+      Write-Verbose "& $env:dnscriptPath $env:BingLocation\bing.csx `"$query`" --max=3" -Verbose:$Verbose
+      & $env:dnscriptPath $env:BingLocation\bing.csx "$query" --max=3
+    }
+
+    Set-Alias -Name bing -Value Search-Bing -PassThru
+  }
 }
 catch {
   Write-Host $Error    
