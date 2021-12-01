9b3edeeaef24cd6e25bfe4bbe1f96821aa1fbac2
The Sharp Ninja <ninja@thesharp.ninja>
2021-11-30 20:48:54 -0600

    final edit?

--- linux-setup.sh
+++ linux-setup.sh
@@ -66,15 +66,6 @@ then
 
         cd $destination;
 
-        # GIT_TRACE=true \
-        # GIT_CURL_VERBOSE=true \
-        # GIT_SSH_COMMAND="ssh -vvv" \
-        # GIT_TRACE_PACK_ACCESS=true \
-        # GIT_TRACE_PACKET=true \
-        # GIT_TRACE_PACKFILE=true \
-        # GIT_TRACE_PERFORMANCE=true \
-        # GIT_TRACE_SETUP=true \
-        # GIT_TRACE_SHALLOW=true \
         git clone https://github.com/sharpninja/Snippets.git
 
         cp $destination/Snippets/Linux-ReadmeTest.ps9 $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -98,15 +89,6 @@ else
 
     cd $destination;
 
-    # GIT_TRACE=true \
-    # GIT_CURL_VERBOSE=true \
-    # GIT_SSH_COMMAND="ssh -vvv" \
-    # GIT_TRACE_PACK_ACCESS=true \
-    # GIT_TRACE_PACKET=true \
-    # GIT_TRACE_PACKFILE=true \
-    # GIT_TRACE_PERFORMANCE=true \
-    # GIT_TRACE_SETUP=true \
-    # GIT_TRACE_SHALLOW=true \
     git clone https://github.com/sharpninja/Snippets.git
 
     cp $destination/Snippets/Linux-ReadmeTest.ps9 $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
