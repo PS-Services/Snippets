a4f6cd46933b6823b0b39163d021e66f90e563f5
The Sharp Ninja <ninja@thesharp.ninja>
2021-12-06 15:56:44 -0600

    Fixed version bug.

--- set-version.ps1
+++ set-version.ps1
@@ -2,23 +2,33 @@ using namespace System
 
 param([switch]$Verbose = $false)
 Push-Location
-if((Get-Location).Path.EndsWith("PowerShell")) { Set-Location Snippets }
+Push-Location
+try {
+    if ((Get-Location).Path.EndsWith('PowerShell')) {
+        Set-Location Snippets
+    }
 
-$gitversion = Get-Command dotnet-gitversion -Verbose:$Verbose -ErrorAction SilentlyContinue
+    $gitversion = Get-Command dotnet-gitversion -Verbose:$Verbose -ErrorAction SilentlyContinue
 
-if(-not $gitversion) {
-    & dotnet tool install gitversion.tool -g
+    if (-not $gitversion) {
+        & dotnet tool install gitversion.tool -g
+    }
+
+    $gitversion = Get-Command dotnet-gitversion -Verbose:$Verbose -ErrorAction SilentlyContinue
+
+    if (-not $gitversion) {
+        throw 'Cannot find or install dotnet-gitversion.'
+    }
+
+    $version = & $gitversion /showvariable FullSemVer
+
+    $version | Out-File -FilePath '.version' -Encoding utf8 -Force
+
+    Write-Verbose "Current Semver is $version" -Verbose:$Verbose
 }
-
-$gitversion = Get-Command dotnet-gitversion -Verbose:$Verbose -ErrorAction SilentlyContinue
-
-if(-not $gitversion) {
-    throw "Cannot find or install dotnet-gitversion."
+catch {
+    write-error $_
 }
-
-$version = & $gitversion /showvariable FullSemVer
-
-$version | Out-File -FilePath '.version' -Encoding utf8NoBOM -force
-
-Write-Verbose "Current Semver is $version" -Verbose:$Verbose
-Pop-Location
+finally {
+    Pop-Location
+}
