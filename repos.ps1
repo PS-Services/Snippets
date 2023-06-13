b162b32b70e95ccfc24b915e889e5adddee7f8b1
sharpninja <ninja@thesharp.ninja>
2023-06-12 19:37:29 -0500

    Refinements of repository snippets.

--- repos.ps1
+++ repos.ps1
@@ -1,14 +1,41 @@
+using namespace System.Collections.Generic
+using namespace System.IO
+using namespace System.Linq
+using namespace System.Text.RegularExpressions
+
 param([switch]$VerboseSwitch = $false)
 
 # $Verbose=$true -or $VerboseSwitch
-$Verbose=$VerboseSwitch
+$Verbose = $VerboseSwitch
 # Write-Verbose "[$script] [$env:SnippetsInitialized] -not `$env:SnippetsInitialized: $(-not $env:SnippetsInitialized)" -Verbose:$Verbose
 $script = $MyInvocation.MyCommand
 
 Write-Verbose $MyInvocation
 
+class ResultItem {
+  [string]$Repo;
+  [string]$Command;
+  [string]$ID;
+  [string]$Version;
+  [string]$Name;
+
+  ResultItem(
+    [string]$r,
+    [string]$i,
+    [string]$v,
+    [string]$n,
+    [string]$ins
+  ) {
+    $this.Repo = $r;
+    $this.ID = $i;
+    $this.Version = $v;
+    $this.Name = $n;
+    $this.Command = $ins;
+  }
+}
+
 if (-not $env:SnippetsInitialized) {
-  $fileInfo = New-Object System.IO.FileInfo (Get-Item $PSScriptRoot).FullName
+  $fileInfo = New-Object FileInfo (Get-Item $PSScriptRoot).FullName
   $path = $fileInfo.Directory.FullName;
   . $path/Snippets/common.ps1;
   Initialize-Snippets -Verbose:$Verbose
@@ -16,188 +43,419 @@ if (-not $env:SnippetsInitialized) {
 
 if ($env:IsWindows -eq 'true') {
   try {
-    function Call-Winget {
-      [CmdletBinding(PositionalBinding = $false)]
+    function Invoke-Winget {
+      [CmdletBinding(PositionalBinding = $True)]
       param(
-        [string]$Command = 'search',
-        [string]$Name = $null,
+        [Parameter(Position = 0)][string]$Command = 'search',
+        [Parameter(Position = 1)][string]$Name = $null,
         [string]$Store = 'winget',
         [switch]$Install = $false,
-        [switch]$Interactive = $false
+        [switch]$Interactive = $false,
+        [switch]$AllRepos = $false
       )
 
+      if ($Name -eq '' -and $Command) {
+        if (-not($Command -imatch 'list|upgrade|update')) {
+          $Name = $Command;
+          $Command = 'search';
+        }
+        
+        if($Command -ieq "upgrade"){
+          $Name = "--all"
+        }
+    }
+
       if ($Install) {
-        $Command = 'install'
+        $Command = 'search'
       }
 
       $params = @()
 
-      switch($Command.ToLower()){
-        ((-eq "search") -or (-eq "find")) {
+      Switch -regex ($Command) {
+        ('search|find') {
           $params += "search";
-          $params += "-q";
           $params += $Name;
-          $params += "-s";
-          $params += "winget";
         }
 
-        (-eq "install") {
+        ('install') {
           $params += "install";
-          $params += "-q";
           $params += $Name;
-          $params += "-s";
-          $params += "winget";
         }
 
-        (-eq "upgrade") {
+        ('upgrade') {
           $params += "upgrade";
-          $params += "-q";
           $params += $Name;
-          $params += "-s";
-          $params += "winget";
         }
 
-        ((-eq "uninstall") -or (-eq "remove")) {
+        ('uninstall|remove') {
           $params += "uninstall";
-          $params += "-q";
           $params += $Name;
-          $params += "-s";
-          $params += "winget";
         }
 
-        ((-eq "show") -or (-eq "details") -or (-eq "info")) {
+        ('show|details|info') {
           $params += "show";
-          $params += "-q";
           $params += $Name;
-          $params += "-s";
-          $params += "winget";
         }
 
-        ((-eq "update") -or (-eq "refresh")) {
+        ('update|refresh') {
           $params += "update";
-          $params += "-s";
-          $params += "winget";
         }
 
         default {
           $params += $Command;
           $params += $Name;
-          $params += "-s";
-          $params += "winget";
         }
       }
 
-      Write-Verbose "[$script] Parameters: [$params]"
+      if ($Store) {
+        $params += "-s";
+        $params += $Store;
+      }
+
+      Write-Verbose "[$script] Parameters: [$params]" -Verbose:$Verbose
 
       $winGet = Get-Command winget.exe
 
       if ($winGet) {
-        $i = if ($Interactive) {
-          '-i'
+        if ($Interactive) {
+          $params += "-i"
+        }
+
+        # Write-Verbose "[$script] & $winGet $Command $Name -s $Store $i;"
+        Write-Verbose "[$script] & $winGet $params" -Verbose:$Verbose
+        $wingetResults = & $winGet $params
+        & refreshenv
+
+        $results = @('Begin winget', '')
+
+        [Object[]]$wingetResults = $wingetResults.Split("`n");
+        [List[ResultItem]]$wingetList = [List[ResultItem]]::new();
+  
+        $regex = [Regex]::new('^.+\s+([A-Za-z0-9_-]+\.[A-Za-z0-9_-]+)\s+([\d\.]+)\s?');
+  
+        foreach ($line in $wingetResults) {
+          if ($regex.IsMatch($line)) {
+            $id = $regex.Match($line).Groups[1].Value.Trim();
+            $ver = $regex.Match($line).Groups[2].Value.Trim();
+            $index = $line.IndexOf($id);
+            $nme = $line.Substring(0, $index).Trim();
+            switch -regex ($Command) {
+              'search' { $inst = "install $id --version $ver --source $Store $($Interactive ? '-i' : '')".Trim() }
+              'list' { $inst = "uninstall $id" }
+            }
+            
+            $wingetList.Add([ResultItem]::new(
+                "winget", $id, $ver, $nme, $inst
+              ));
+          }
+        }
+  
+        $results += $wingetResults
+        $results += @('End winget', '')
+
+        if ($Command -imatch 'search|list' ) {
+          $firstItem = @([Enumerable]::OfType[ResultItem]($wingetList))[0];
+
+          if ($Install -and $firstItem) {
+            $arguments = $firstItem.Command.Split(' ')
+            & $firstItem.Repo $arguments
+          }
+
+          if ($AllRepos.IsPresent -and -not $AllRepos) {
+            $wingetList | Format-Table -AutoSize  
+          }
+          elseif (-not $AllRepos.IsPresent) {
+            $wingetList | Format-Table -AutoSize  
+          }
+          else {
+            $wingetList
+          }
         }
         else {
-          ''
-        }
-        Write-Verbose "[$script] & $winGet $Command $Name -s $Store $i;"
-        & $winGet $Command $Name -s $Store $i;
-        & refreshenv
+          $results
+        }      
       }
     }
 
-    function Call-Scoop {
-      [CmdletBinding(PositionalBinding = $false)]
-      param(
-        [string]$Command = 'list',
+    function Invoke-Scoop {
+      [CmdletBinding(PositionalBinding = $True)]
+      param(        
+        [Parameter(Position = 0)][string]$Command = 'search',        
+        [Parameter(Position = 1)][string]$Name = $null,
         [string]$SubCommand = $null,
-        [string]$Name = $null
+        [switch]$Install = $false,
+        [switch]$AllRepos = $false
       )
 
-      if (-not $Name -and $SubCommand) {
-        $Name = $SubCommand;
-        $SubCommand = $null;
+      if ($Name -eq '' -and -not($Command -imatch 'list|upgrade')) {
+        $Name = $Command;
+        $Command = 'search';
       }
+
+      if ($Install) {
+        $Command = 'search'
+      }
+
+      [Object[]]$params = @()
+
+      Switch -regex ($Command) {
+        ('search|find') {
+          $params += "search";
+          if ($SubCommand) { $params += $SubCommand.Trim() }
+          $params += $Name;
+        }
+
+        ('install') {
+          $params += "install";
+          if ($SubCommand) { $params += $SubCommand.Trim() }
+          $params += $Name;
+        }
+
+        ('upgrade|update') {
+          $params += "update";
+          if ($SubCommand) { $params += $SubCommand.Trim() }
+          $params += $Name;
+        }
+
+        ('uninstall|remove') {
+          $params += "uninstall";
+          if ($SubCommand) { $params += $SubCommand.Trim() }
+          $params += $Name;
+        }
+
+        ('show|details|info') {
+          $params += "info";
+          if ($SubCommand) { $params += $SubCommand.Trim() }
+          $params += $Name;
+        }
+
+        default {
+          $params += $Command;
+          if ($SubCommand) { $params += $SubCommand.Trim(); }
+          if ($Name) { $params += $Name; }
+        }
+      }
+
+      [Object[]] $params = @($params)
+
+      Write-Verbose "[$script] Parameters: [$params]" -Verbose:$Verbose
 
       $scoop = Get-Command scoop
 
       if ($scoop) {
-        $SubCommand = if ($SubCommand) {
-          $SubCommand
+        Write-Verbose "[$script] Invoke-Expression `"& `"$($scoop.Source)`" $params`"" -Verbose:$Verbose
+        $scoopResults = Invoke-Expression "& `"$($scoop.Source)`" $params"
+        & refreshenv      
+      
+        $results = @('Begin scoop', '')
+
+        [List[object]]$scoopList = [List[object]]::new();
+  
+        foreach ($line in $scoopResults) {
+          $ver = $line.version;
+          $nme = $line.name;
+          switch -regex ($Command) {
+            'search' { 
+              if ($ver) {
+                $inst = "install $($nme)@$($ver) $($line.Bucket ? "--bucket $($line.bucket)" : '')" 
+              }
+              else {
+                $inst = "install $nme $($line.Bucket ? "--bucket $($line.bucket)" : '')" 
+              }
+            }
+            'list' { $inst = "uninstall $nme" }
+          }
+          $scoopList.Add([ResultItem]::new(
+              "scoop", $nme, $ver, $nme, $inst
+            ));
         }
+        
+        $results += $scoopResults
+        $results += @('End scoop', '')
+
+        if ($Command -imatch 'search|list' ) {
+          $firstItem = @([Enumerable]::OfType[ResultItem]($scoopList))[0];
+
+          if ($Install -and $firstItem) {
+            $arguments = $firstItem.Command.Split(' ')
+            & $firstItem.Repo $arguments
+          }
+          
+          if ($AllRepos.IsPresent -and -not $AllRepos) {
+            $scoopList | Format-Table -AutoSize  
+          }
+          elseif (-not $AllRepos.IsPresent) {
+            $scoopList | Format-Table -AutoSize  
+          }
+          else {
+            $scoopList
+          }        }
         else {
-          ''
-        }
-        $c = "$Command $SubCommand".Trim()
-        Write-Verbose "[$script] . $scoop $c $Name;"
-        . $scoop $c $Name;
-        & refreshenv
+          $results
+        }      
       }
     }
 
-    function Call-Choco {
-      [CmdletBinding(PositionalBinding = $false)]
+    function Invoke-Choco {
+      [CmdletBinding(PositionalBinding = $True)]
       param(
-        [string]$Command = 'list',
+        [Parameter(Position = 0)][string]$Command = 'list',
+        [Parameter(Position = 1)][string]$Name = $null,
         [string]$SubCommand = $null,
-        [string]$Name = $null
+        [switch]$Install = $false,
+        [switch]$AllRepos = $false
       )
 
-      if (-not $Name -and $SubCommand) {
-        $Name = $SubCommand;
-        $SubCommand = $null;
+      if ($Name -eq '' -and -not($Command -imatch 'list|upgrade')) {
+        $Name = $Command;
+        $Command = 'search';
       }
+
+      if ($Install) {
+        $Command = 'search'
+      }
+
+      $params = @()
+
+      Switch -regex ($Command) {
+        ('search|find') {
+          $params += "search";
+          if ($SubCommand) { $params += $SubCommand.Trim() }
+          $params += $Name;
+        }
+
+        ('install') {
+          $params += "install";
+          if ($SubCommand) { $params += $SubCommand.Trim() }
+          $params += $Name;
+        }
+
+        ('upgrade|update') {
+          $params += "upgrade";
+          if ($SubCommand) { $params += $SubCommand.Trim() }
+          $params += $Name;
+        }
+
+        ('uninstall|remove') {
+          $params += "uninstall";
+          if ($SubCommand) { $params += $SubCommand.Trim() }
+          $params += $Name;
+        }
+
+        ('show|details|info') {
+          $params += "info";
+          if ($SubCommand) { $params += $SubCommand.Trim() }
+          $params += $Name;
+        }
+
+        default {
+          $params += $Command;
+          if ($SubCommand) { $params += $SubCommand.Trim(); }
+          if ($Name) { $params += $Name; }
+        }
+      }
+
+      Write-Verbose "[$script] Parameters: [$params]" -Verbose:$Verbose
 
       $choco = Get-Command choco
 
       if ($choco) {
-        $SubCommand = if ($SubCommand) {
-          $SubCommand
+        Write-Verbose "[$script] & $choco $params" -Verbose:$Verbose
+        $chocoResults = & $choco $params
+        & refreshenv
+
+        $results = @('Begin chocolatey', '')
+
+        [Object[]]$chocoResults = $chocoResults.Split("`n");
+        [List[object]]$chocoList = [List[object]]::new();
+  
+        foreach ($line in $chocoResults) {
+          if ($line.IndexOf('[Approved]') -gt -1) {
+            $line = $line.Substring(0, $line.IndexOf('[Approved]')).Trim();
+            $lastIndex = $line.LastIndexOf(' ');
+            $nme = $line.Substring(0, $lastIndex).Trim();
+            $ver = $line.Substring($lastIndex).Trim();
+            switch -regex ($Command) {
+              'search' { $inst = "install $nme --version $ver -y" }
+              'list' { $inst = "uninstall $nme" }
+            }
+            $chocoList.Add([ResultItem]::new(
+                "choco", $nme, $ver, $nme, $inst
+              ));
+          }
+        }
+  
+        $results += $chocoResults
+        $results += @('End chocolatey', '')
+
+        if ($Command -imatch 'search|list' ) {
+          $firstItem = @([Enumerable]::OfType[ResultItem]($chocoList))[0];
+
+          if ($Install -and $firstItem) {
+            $arguments = $firstItem.Command.Split(' ')
+            & $firstItem.Repo $arguments
+          }
+
+          if ($AllRepos.IsPresent -and -not $AllRepos) {
+            $chocoList | Format-Table -AutoSize  
+          }
+          elseif (-not $AllRepos.IsPresent) {
+            $chocoList | Format-Table -AutoSize  
+          }
+          else {
+            $chocoList
+          }
         }
         else {
-          ''
+          $results
         }
-        $c = "$Command $SubCommand".Trim()
-        Write-Verbose "[$script] sudo $choco $c $Name -y -PassThru;"
-        sudo $choco $c $Name -y -PassThru;
-        & refreshenv
       }
     }
 
-    function Call-All {
+    function Invoke-All {
       [CmdletBinding(PositionalBinding = $true)]
       param(
-        [string]$Command = 'search',
+        [Parameter(Position = 0)][string]$Command = 'search',
+        [Parameter(Position = 1)][string]$Name = $null,
         [string]$SubCommand = $null,
-        [string]$Name = $null,
         [string]$Store = 'winget',
+        [switch]$Install = $false,
         [switch]$Interactive = $false
       )
 
-      if (-not $Name) {
-        if ($SubCommand) {
-          $Name = $SubCommand;
-          $SubCommand = $null;
-        }
-        elseif ($Command) {
-          $Name = $Command;
-          $Command = 'search';
-        }
+      if ($Name -eq '' -and -not($Command -imatch 'list|upgrade')) {
+        $Name = $Command;
+        $Command = 'search';
       }
 
       $results = @()
-      $results += Call-Winget -Command $Command -Name $Name -Store $Store -Interactive:$Interactive
-      $results += @('End winget', '')
-      $results += Call-Scoop -Command $Command -Subcommand $Subcommand -Name $Name
-      $results += @('End scoop', '')
-      $results += Call-Choco -Command $Command -Subcommand $Subcommand -Name $Name
-      $results += @('End chocolatey', '')
+      $wingetResults = Invoke-Winget $Command $Name -Store $Store -Interactive:$Interactive -AllRepos
+      $scoopResults = Invoke-Scoop $Command $Name -Subcommand $Subcommand -AllRepos
+      $chocoResults += Invoke-Choco $Command $Name -Subcommand $Subcommand -AllRepos
 
-      return $results
+      $results = $wingetResults
+      $results += $scoopResults
+      $results += $chocoResults
+
+      if ($Command -imatch 'search|list' ) {
+        $results | Format-Table -Property Repo, Command -GroupBy Repo -AutoSize
+      }
+      else {
+        $results
+      }
+
+      $firstItem = @([Enumerable]::OfType[ResultItem]($results))[0];
+
+      if ($Install -and $firstItem) {
+        $arguments = $firstItem.Command.Split(' ')
+        & $firstItem.Repo $arguments
+      }
     }
 
-    $alias = set-alias -Verbose:$Verbose -Scope Global -Description "Snippets: [repos] WinGet" -Name wg -Value Call-Winget -PassThru
-    $alias = set-alias -Verbose:$Verbose -Scope Global -Description "Snippets: [repos] Scoop" -Name scp -Value Call-Scoop -PassThru
-    $alias = set-alias -Verbose:$Verbose -Scope Global -Description "Snippets: [repos] Chocolatey" -Name ch -Value Call-Choco -PassThru
-    $alias = set-alias -Verbose:$Verbose -Scope Global -Description "Snippets: [repos] All Repos" -Name repos -Value Call-All -PassThru
+    set-alias -Verbose:$Verbose -Scope Global -Description "Snippets: [repos] WinGet" -Name wg -Value Invoke-Winget -PassThru
+    set-alias -Verbose:$Verbose -Scope Global -Description "Snippets: [repos] Scoop" -Name scp -Value Invoke-Scoop -PassThru
+    set-alias -Verbose:$Verbose -Scope Global -Description "Snippets: [repos] Chocolatey" -Name ch -Value Invoke-Choco -PassThru
+    set-alias -Verbose:$Verbose -Scope Global -Description "Snippets: [repos] All Repos" -Name repos -Value Invoke-All -PassThru
 
     return "Repos aliases configured."
   }
@@ -208,7 +466,8 @@ if ($env:IsWindows -eq 'true') {
     Write-Verbose '[repos.ps1] Leaving...' -Verbose:$Verbose
     $Verbose = $VerboseSwitch
   }
-} else {
+}
+else {
   $Verbose = $VerboseSwitch
   return "Wrong Operating System."
 }
